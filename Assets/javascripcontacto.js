e006c2cbe897c53f235833aa3c7b41578e2c447e
Heralfso <hernan244s@gmail.com>
2021-09-28 01:20:59 -0500

    Avance

---
+++ Assets/javascripcontacto.js
@@ -0,0 +1,15 @@
+function ValidFields() {
+    var firstName = document.getElementById("firsName").Value;
+    var secondName = document.getElementById("secondName").Value;
+    var lastName = document.getElementById("lastName").Value;
+    var secondLastName = document.getElementById("secondLastName").Value;
+    var age = document.getElementById("age").Value;
+    var gender = document.getElementById("gender").Value;
+
+    var errorFirstName = document.getElementById("errorFirstName")
+    var errorSecondName = document.getElementById("errorSecondName")
+    var errorLastName = document.getElementById("errorLastName")
+    var errorSecondLastName = document.getElementById("errorSecondLastName")
+    var errorAge = document.getElementById("errorAge")
+    var ErrorGender = document.getElementById("errorGender")
+}
