ae1de1f74170dbbfa234eec12c0c53e2fc26ab45
Heralfso <hernan244s@gmail.com>
2021-09-29 23:22:42 -0500

    Continue

--- Assets/javascripcontacto.js
+++ Assets/javascripcontacto.js
@@ -1,10 +1,10 @@
 function ValidFields() {
-    var firstName = document.getElementById("firsName").Value;
-    var secondName = document.getElementById("secondName").Value;
-    var lastName = document.getElementById("lastName").Value;
-    var secondLastName = document.getElementById("secondLastName").Value;
-    var age = document.getElementById("age").Value;
-    var gender = document.getElementById("gender").Value;
+    var firstName = document.getElementById("firsName").value;
+    var secondName = document.getElementById("secondName").value;
+    var lastName = document.getElementById("lastName").value;
+    var secondLastName = document.getElementById("secondLastName").value;
+    var age = document.getElementById("age").value;
+    var gender = document.getElementById("gender").value;
 
     var errorFirstName = document.getElementById("errorFirstName")
     var errorSecondName = document.getElementById("errorSecondName")
@@ -12,4 +12,55 @@ function ValidFields() {
     var errorSecondLastName = document.getElementById("errorSecondLastName")
     var errorAge = document.getElementById("errorAge")
     var ErrorGender = document.getElementById("errorGender")
+
+    if(firstName === "" || firstName.length > 10) {
+        errorFirstName.innerText = "Name empty or size greater thand 10";
+        return;
+    } else {
+        errorFirstName.innerText="";
+    }
+
+    if(SecondName.length > 10) {
+        errorSecondName.innerText = "size greater thand 10";
+        return;
+    } else {
+        errorSecondName.innerText="";
+    }
+
+    if(isNaN(Number(age))) { 
+        errorAge.innerText = "age is not number";
+    }
+
+    if(Number(age) < 0 || Number(age) > 100){
+        errorAge.innerText = "Invalid age";
+        return;
+    } else {
+        errorAge.innerText = "";
+    }
+
+    if(gender === "e"){
+        ErrorGender.innerText = "Invalid age"
+    }
+
+
+    if(LastName.length > 10) {
+        errorlastName.innerText = "size greater thand 10";
+        return;
+    } else {
+        errorlastName.innerText="";
+    }
+
+    if(SecondLastName.length > 10) {
+        SecondLastName.innerText = "size greater thand 10";
+        return;
+    } else {
+        errorSecondLastName.innerText="";
+    }
+
+    if(Age > 100) {
+        errorAge.innerText = "Invalid age";
+        return;
+    } else {
+        errorAge.innerText="";
+    }
 }
